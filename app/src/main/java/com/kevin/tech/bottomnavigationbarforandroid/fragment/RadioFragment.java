9c2a3c45db4f9bd6bf0ea3cde189afac5dc860e4
student9128 <student9128@gmail.com>
2016-11-29 11:11:21 +0800

    add switch fragment for RadioFragment

--- app/src/main/java/com/kevin/tech/bottomnavigationbarforandroid/fragment/RadioFragment.java
+++ app/src/main/java/com/kevin/tech/bottomnavigationbarforandroid/fragment/RadioFragment.java
@@ -3,8 +3,10 @@ package com.kevin.tech.bottomnavigationbarforandroid.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,10 @@ import android.widget.TextView;
 
 import com.kevin.tech.bottomnavigationbarforandroid.Constants;
 import com.kevin.tech.bottomnavigationbarforandroid.R;
+import com.kevin.tech.bottomnavigationbarforandroid.fragment.subfragment.HomeFragment;
+import com.kevin.tech.bottomnavigationbarforandroid.fragment.subfragment.LikeFragment;
+import com.kevin.tech.bottomnavigationbarforandroid.fragment.subfragment.LocationFragment;
+import com.kevin.tech.bottomnavigationbarforandroid.fragment.subfragment.PersonFragment;
 
 /**
  * Created by Kevin on 2016/11/28.
@@ -26,6 +32,10 @@ public class RadioFragment extends Fragment implements RadioGroup.OnCheckedChang
     private TextView mTextView;
     private RadioButton mRadioHome, mRadioLocation, mRadioLike, mRadioMe;
     private RadioGroup mRadioGroup;
+    private HomeFragment mHomeFragment;
+    private LocationFragment mLocationFragment;
+    private LikeFragment mLikeFragment;
+    private PersonFragment mPersonFragment;
 
     public static RadioFragment newInstance(String s) {
         RadioFragment radioFragment = new RadioFragment();
@@ -53,27 +63,61 @@ public class RadioFragment extends Fragment implements RadioGroup.OnCheckedChang
             }
         }
         mRadioGroup.setOnCheckedChangeListener(this);
-        mRadioHome.setChecked(true);
-        setHomeState();
+        Log.i("Kevin", "执行了");
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        setDefaultFragment();//写在onCreateView里面，当页面跑到其他Fragment再回来就不会生效
+        super.onStart();
+    }
+
+    private void setDefaultFragment() {
+        mRadioHome.setChecked(true);
+        mRadioLocation.setChecked(false);
+        mRadioLike.setChecked(false);
+        mRadioMe.setChecked(false);
+        if (mRadioHome.isChecked()) {
+            setTabState();
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            mHomeFragment = mHomeFragment.newInstance(getString(R.string.item_home));
+            transaction.replace(R.id.sub_content, mHomeFragment).commit();
+        }
     }
 
 
     @Override
     public void onCheckedChanged(RadioGroup group, int checkId) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
         switch (checkId) {
             case R.id.rb_home:
-//                setHomeState();
+                if (mHomeFragment == null) {
+                    mHomeFragment = HomeFragment.newInstance(getString(R.string.item_home));
+                }
+                transaction.replace(R.id.sub_content, mHomeFragment);
                 break;
             case R.id.rb_location:
-//                setLocationState();
+                if (mLocationFragment == null) {
+                    mLocationFragment = LocationFragment.newInstance(getString(R.string.item_location));
+                }
+                transaction.replace(R.id.sub_content, mLocationFragment);
                 break;
             case R.id.rb_like:
+                if (mLikeFragment == null) {
+                    mLikeFragment = LikeFragment.newInstance(getString(R.string.item_like));
+                }
+                transaction.replace(R.id.sub_content, mLikeFragment);
                 break;
             case R.id.rb_me:
+                if (mPersonFragment == null) {
+                    mPersonFragment = PersonFragment.newInstance(getString(R.string.item_person));
+                }
+                transaction.replace(R.id.sub_content, mPersonFragment);
                 break;
         }
         setTabState();
+        transaction.commit();
     }
 
     private void setTabState() {
@@ -84,19 +128,22 @@ public class RadioFragment extends Fragment implements RadioGroup.OnCheckedChang
 
     }
 
-    private void setLocationState() {
-        if (mRadioLocation.isChecked()) {
-            mRadioLocation.setTextColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
-        } else {
-            mRadioLocation.setTextColor(ContextCompat.getColor(getActivity(), R.color.black));
-        }
-    }
-
+    /**
+     * set tab home state
+     */
     private void setHomeState() {
         if (mRadioHome.isChecked()) {
             mRadioHome.setTextColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
         } else {
             mRadioHome.setTextColor(ContextCompat.getColor(getActivity(), R.color.black));
+        }
+    }
+
+    private void setLocationState() {
+        if (mRadioLocation.isChecked()) {
+            mRadioLocation.setTextColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
+        } else {
+            mRadioLocation.setTextColor(ContextCompat.getColor(getActivity(), R.color.black));
         }
     }
 
@@ -115,4 +162,5 @@ public class RadioFragment extends Fragment implements RadioGroup.OnCheckedChang
             mRadioMe.setTextColor(ContextCompat.getColor(getActivity(), R.color.black));
         }
     }
+
 }
